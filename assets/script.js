0ccbda09022063cc218cdf285beeb3aa6eb79a08
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-01-10 14:53:50 -0800

    function to save to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,46 @@
 let container = $(".container");
 let body = $("body");
 let currentDate = $("#currentDay")
-let timeFormat = moment().format("dddd, MMMM Do YYYY");
-
-
+let timeFormat = moment().format("dddd, MMMM Do");
+let currentHour = moment().hour();
+let textarea = $("textarea");
+let saveBtn = $(".saveBtn");
 
 $( document ).ready(function() {
     console.log( "document loaded" );
 
+    function displayTime(){
+        currentDate.text(timeFormat) 
+        }
 
-function displayTime(){
-currentDate.text(timeFormat) 
-
-}
-
+    $(textarea).each(function(){
+        let timeblockColor = parseInt($(this).attr("id"));
+        console.log(timeblockColor);
+        
+        if (timeblockColor === currentHour){
+            $(this).addClass("present")
+        } else if (timeblockColor > currentHour){
+            $(this).addClass("future")
+        } else if (timeblockColor < currentHour){
+            $(this).addClass("past")
+        }
+    })
 displayTime();
 });
 
+function setStorage(){
+    $(textarea).each(function(){
+    let textareaTime = $(this).attr("id");
+    let textareaTask = $(this).val();
+    
+    localStorage.setItem(textareaTime, textareaTask)
 
+    console.log(textareaTask);
+    console.log(textareaTime);
+})
+}
+
+
+(saveBtn).on("click", setStorage);
 
 
